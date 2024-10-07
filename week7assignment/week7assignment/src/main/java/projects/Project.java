c0c990bc3733608d0e8780a74d710f2c5824741f
bhorvic <123758379+bhorvic@users.noreply.github.com>
2024-10-07 17:39:12 -0600

    Add files via upload

---
+++ week7assignment/week7assignment/src/main/java/projects/Project.java
@@ -0,0 +1,16 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class Project {
+
+	public Project() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+
+	}
+
+}
